a5ee68746ee73f442cb4fd5ac8f78f5f728e966d
Akash Jangra <aj.akashjangra@gmail.com>
2025-01-27 07:28:50 +0530

    db connection - mongodb

---
+++ src/index.js
@@ -0,0 +1,36 @@
+// require('dotenv').config({path: './env'});
+
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+import { DB_NAME } from './constants.js';
+import connectDB from './db/index.js';
+
+dotenv.config({
+  path: './env'
+})
+
+connectDB();
+
+/*
+import express from "express";
+const app = express();
+
+//IIFE or immediately invoked function expression
+(async () => {
+  try {
+    await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+    app.on("error", (error) => {
+      console.log("ERR: ", error);
+      throw error;
+    });
+
+    app.listen(process.env.PORT, () => {
+      console.log(`Server is running on port ${process.env.PORT}`);
+    });
+  } catch (error) {
+    console.error("Error: ", error);
+    throw err
+  }
+})();
+
+*/

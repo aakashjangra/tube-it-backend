6d91d310f59ace4e4ff2f535151ceb901da5e93c
Akash Jangra <aj.akashjangra@gmail.com>
2025-01-28 10:38:46 +0530

    add user and video model

---
+++ src/models/user.model.js
@@ -0,0 +1,88 @@
+import mongoose, {Schema, model} from 'mongoose'
+
+const userSchema = new Schema({
+  username: {
+    type: String, 
+    required: true,
+    unique: true,
+    lowercase: true, 
+    trim: true,
+    index: true
+    //index makes searching for that field optimized in mongodb
+  }, 
+  email: {
+    type: String, 
+    required: true,
+    unique: true,
+    lowercase: true, 
+    trim: true
+  }, 
+  fullname: {
+    type: String, 
+    required: true,
+    trim: true,
+    index: true
+  }, 
+  avatar: {
+    type: String, //cloudinary url
+    required: true
+  }, 
+  coverImage: {
+    type: String, 
+  }, 
+  watchHistory: [
+    {
+      type: Schema.Types.ObjectId, 
+      ref: 'Video'
+    }
+  ], 
+  password: {
+    type: String,
+    required: [true, "Password is required"]
+  },
+  refreshToken: {
+    type: String
+  }
+}, {timestamps: true})
+//timestamps: true - adds createdAt and updatedAt fields to the schema
+
+//executes just before the document is saved
+userSchema.pre("save", async function (next) {
+  if(this.isModified("password")){
+    this.password = await bcrypt.hash(this.password, 10);
+  }
+  next();
+})
+
+userSchema.methods.isPasswordCorrect = async function (password) {
+  return await bcrypt.compare(password, this.password);
+}
+
+userSchema.methods.generateAccessToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+      email: this.email,
+      username: this.username,
+      fullname: this.fullname,
+    }, 
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+      expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+    }
+  )
+}
+userSchema.methods.generateRefreshToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+      expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+    }
+  )
+}
+
+export const User = model('User', userSchema)
+//in mognogdb - "users"

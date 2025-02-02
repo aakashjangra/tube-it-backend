f7a4f514ad7b2c902e8c6b6b22a3ff5efe702984
Akash Jangra <aj.akashjangra@gmail.com>
2025-02-02 13:04:33 +0530

    add subscription model

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -162,43 +162,43 @@ const logoutUser = asyncHandler(async (req, res) => {
 const refreshAccessToken = asyncHandler(async (req, res) => {
   const incomingRefreshToken = req.cookies.refreshToken || req.body.refreshToken
 
-  if(!incomingRefreshToken){
+  if (!incomingRefreshToken) {
     throw new ApiError(401, "unauthorized request");
   }
 
   try {
-    const decodedToken = jwt.verify( 
+    const decodedToken = jwt.verify(
       incomingRefreshToken,
       process.env.REFRESH_TOKEN_SECRET
     );
-  
+
     const user = await User.findById(decodedToken?._id)
-  
-    if(!user){
+
+    if (!user) {
       throw new ApiError(401, "Invalid refresh token")
     }
-  
-    if(incomingRefreshToken !== user?.refreshToken){
+
+    if (incomingRefreshToken !== user?.refreshToken) {
       throw new ApiError(401, "Refresh token is expired or used")
     }
-  
+
     const options = {
-      httpOnly: true, 
+      httpOnly: true,
       secure: true
     }
-    const {accessToken, newRefreshToken} = await generateAccessAndRefreshTokens(user._id);
-  
+    const { accessToken, newRefreshToken } = await generateAccessAndRefreshTokens(user._id);
+
     return res
-    .status(200)
-    .cookie("accessToken", accessToken, options)
-    .cookie("refreshToken", newRefreshToken, options)
-    .json(
-      new ApiResponse(
-        200, 
-        {accessToken, refreshToken: newRefreshToken}, 
-        "Access token refreshed"
+      .status(200)
+      .cookie("accessToken", accessToken, options)
+      .cookie("refreshToken", newRefreshToken, options)
+      .json(
+        new ApiResponse(
+          200,
+          { accessToken, refreshToken: newRefreshToken },
+          "Access token refreshed"
+        )
       )
-    )
   } catch (error) {
     throw new ApiError(401, error?.message || "Invalid refresh token")
   }

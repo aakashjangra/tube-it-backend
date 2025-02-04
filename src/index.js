dd404b7958bd3d37d667eecba7ef7c6bf7fa0e4b
Akash Jangra <aj.akashjangra@gmail.com>
2025-02-04 20:18:48 +0530

    fix server listen after mongodb connection success

--- src/index.js
+++ src/index.js
@@ -2,37 +2,18 @@
 
 import dotenv from 'dotenv';
 import connectDB from './db/index.js';
-import {app} from './app.js';
+import { app } from './app.js';
 
 dotenv.config({
   path: './env'
 })
 
-connectDB();
-
-app.listen(process.env.PORT, () => {
-  console.log(`Server is running on port ${process.env.PORT}`);
-});
-/*
-import express from "express";
-const app = express();
-
-//IIFE or immediately invoked function expression
-(async () => {
-  try {
-    await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
-    app.on("error", (error) => {
-      console.log("ERR: ", error);
-      throw error;
-    });
-
-    app.listen(process.env.PORT, () => {
-      console.log(`Server is running on port ${process.env.PORT}`);
-    });
-  } catch (error) {
-    console.error("Error: ", error);
-    throw err
-  }
-})();
-
-*/
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8001, () => {
+      console.log(`Server is running at port : ${process.env.PORT}`);
+    })
+  })
+  .catch((err) => {
+    console.log("MONGO db connection failed! ", err);
+  })

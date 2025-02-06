21b4c51a71eee271780dec324b1665b380e47615
Akash Jangra <aj.akashjangra@gmail.com>
2025-02-06 14:19:48 +0530

    add comment and video upload controllers

--- src/controllers/comment.controller.js
+++ src/controllers/comment.controller.js
@@ -1,5 +1,7 @@
 import mongoose from "mongoose"
 import {Comment} from "../models/comment.model.js"
+import { Video } from "../models/video.model.js"
+import { User } from "../models/user.model.js"
 import {ApiError} from "../utils/ApiError.js"
 import {ApiResponse} from "../utils/ApiResponse.js"
 import {asyncHandler} from "../utils/asyncHandler.js"
@@ -13,6 +15,33 @@ const getVideoComments = asyncHandler(async (req, res) => {
 
 const addComment = asyncHandler(async (req, res) => {
     // TODO: add a comment to a video
+    const {videoId} = req.params;
+    const {content} = req.body;
+    
+    if(!content || !videoId){
+        throw new ApiError(401, "Content and video id both are required!");
+    }
+
+    const user = await User.findById(req.user?._id);
+
+    if (!user) {
+        throw new ApiError(401, "Invalid user!");
+    }
+
+    const video = await Video.findById(videoId);
+    if (!video) {
+        throw new ApiError(401, "Invalid video id!");
+    }
+    
+    const comment = await Comment.create({
+        content, 
+        video,
+        owner: user
+    })
+
+    return res
+    .status(200)
+    .json(new ApiResponse(200, comment, "Comment added successsfully!"))
 })
 
 const updateComment = asyncHandler(async (req, res) => {
--- src/controllers/video.controller.js
+++ src/controllers/video.controller.js
@@ -14,7 +14,43 @@ const getAllVideos = asyncHandler(async (req, res) => {
 
 const publishAVideo = asyncHandler(async (req, res) => {
     const { title, description} = req.body
+
+    if(!title || !description) {
+        throw new ApiError("Title and description are required!");
+    }
     // TODO: get video, upload to cloudinary, create video
+    const videoLocalPath = req.files?.videoFile[0].path;
+    const thumbnailLocalPath = req.files?.thumbnail[0].path;
+
+    if(!videoLocalPath) {
+        throw new ApiError("Invalid video");
+    }
+
+    if (!thumbnailLocalPath) {
+        throw new ApiError("Invalid thumbnail");
+    }
+
+    const video = await uploadOnCloudinary(videoLocalPath);
+    const thumbnail = await uploadOnCloudinary(thumbnailLocalPath);
+
+    // console.log("video obj is - ", video);
+
+
+    if(!video){
+        throw new ApiError(501, "Error uploading video");
+    }
+
+    const videoDocument = await Video.create({
+        videoFile: video?.url,
+        thumbnail: thumbnail?.url, 
+        title, 
+        description,
+        duration: video.duration
+    })
+
+    return res
+    .status(200)
+    .json(new ApiResponse(200, videoDocument, "Video uploaded successfully"));
 })
 
 const getVideoById = asyncHandler(async (req, res) => {
